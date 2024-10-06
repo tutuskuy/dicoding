57019d95e349d21582eb1d759dd2d2e599c5ee0f
tutuskuy <christoforusstanislaus11@gmail.com>
2024-10-06 10:50:22 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from pathlib import Path
 
 sns.set(style='dark')
 
@@ -44,7 +45,7 @@ def create_temp_df(df):
 
 
 # Load cleaned data
-day1_df = pd.read_csv("day1.csv")
+day1_df = Path(__file__).parents[1]/"dashboard/day1.csv"
 hour1_df = pd.read_csv('hour1.csv')
 
 day1_df['dteday'] = pd.to_datetime(day1_df['dteday'])
